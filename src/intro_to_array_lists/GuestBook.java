20aab31fbc4fd70d947cee28d4f9a5517ee3a539
lelserslasers2 <league@WTS22.attlocal.net>
2019-05-05 15:56:20 -0700

    ArrayLists and GuestBook
    :sunglasses: :thumbsup:

--- src/intro_to_array_lists/GuestBook.java
+++ src/intro_to_array_lists/GuestBook.java
@@ -1,12 +1,49 @@
 package intro_to_array_lists;
 
-public class GuestBook {
-	// Create a GUI with two buttons. One button reads "Add Name" and the other button reads "View Names". 
-	// When the add name button is clicked, display an input dialog that asks the user to enter a name. Add
-	// that name to an ArrayList. When the "View Names" button is clicked, display a message dialog that displays
-	// all the names added to the list. Format the list as follows:
-	// Guest #1: Bob Banders
-	// Guest #2: Sandy Summers
-	// Guest #3: Greg Ganders
-	// Guest #4: Donny Doners
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+
+public class GuestBook implements ActionListener {
+	public void run() {
+		// Create a GUI with two buttons. One button reads "Add Name" and the other
+		// button reads "View Names".
+		JFrame frame = new JFrame();
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame.setVisible(true);
+		JPanel panel = new JPanel();
+		frame.add(panel);
+		JButton addbutton = new JButton();
+		addbutton.setText("Add Button");
+		addbutton.addActionListener(this);
+
+		JButton viewnames = new JButton();
+		viewnames.setText("View Names");
+		viewnames.addActionListener(this);
+
+		panel.add(addbutton);
+		frame.pack();
+		panel.add(viewnames);
+		frame.pack();
+		// When the add name button is clicked, display an input dialog that asks the
+		// user to enter a name. Add
+		// that name to an ArrayList. When the "View Names" button is clicked, display a
+		// message dialog that displays
+		// all the names added to the list. Format the list as follows:
+		// Guest #1: Bob Banders
+		// Guest #2: Sandy Summers
+		// Guest #3: Greg Ganders
+		// Guest #4: Donny Doners
+	}
+
+	@Override
+	public void actionPerformed(ActionEvent e) {
+		JButton buttonClicked = (JButton) e.getSource();
+		if (buttonClicked == addbutton) {
+
+		}
+	}
 }
--- src/intro_to_array_lists/IntroToArrayLists.java
+++ src/intro_to_array_lists/IntroToArrayLists.java
@@ -1,20 +1,47 @@
 package intro_to_array_lists;
 
+import java.util.ArrayList;
+
 public class IntroToArrayLists {
 	public static void main(String[] args) {
-		//1. Create an array list of Strings
-		//   Don't forget to import the ArrayList class
-		
-		//2. Add five Strings to your list
-		
-		//3. Print all the Strings using a standard for-loop
-		
-		//4. Print all the Strings using a for-each loop
-		
-		//5. Print only the even numbered elements in the list.
-		
-		//6. Print all the Strings in reverse order.
-		
-		//7. Print only the Strings that have the letter 'e' in them.
+		// 1. Create an array list of Strings
+		// Don't forget to import the ArrayList class
+		ArrayList<String> strings = new ArrayList<String>();
+		// 2. Add five Strings to your list
+		strings.add("Adam");
+		strings.add("Benjamin");
+		strings.add("Connor");
+		strings.add("Daryl");
+		strings.add("Ethan");
+		// 3. Print all the Strings using a standard for-loop
+		System.out.println("all strings");
+		for (int i = 0; i < strings.size(); i++) {
+			System.out.println(strings.get(i));
+		}
+		// 4. Print all the Strings using a for-each loop
+		System.out.println("all strings");
+		for (String names : strings) {
+			System.out.println(names);
+		}
+		// 5. Print only the even numbered elements in the list.
+		System.out.println("even numbered elements");
+		for (int i = 0; i < strings.size(); i++) {
+			if (i % 2 == 0) {
+				System.out.println(strings.get(i));
+			}
+		}
+		// 6. Print all the Strings in reverse order.
+		System.out.println("reverse order");
+		for (int i = strings.size() - 1; i >= 0; i--) {
+			System.out.println(strings.get(i));
+		}
+		// 7. Print only the Strings that have the letter 'e' in them.
+		System.out.println("contains 5 or more letters");
+		for (int i = 0; i < strings.size(); i++) {
+			if (strings.get(i).length() >= 5) {
+				System.out.println(strings.get(i));
+			}
+
+		}
 	}
 }
