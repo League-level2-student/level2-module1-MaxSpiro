3973c82318b6fe65d12a882530aad5191e73958c
League Student <league@iMac-25.attlocal.net>
2019-05-12 15:42:34 -0700

    i dont know what this is

--- src/intro_to_array_lists/GuestBook.java
+++ src/intro_to_array_lists/GuestBook.java
@@ -2,12 +2,17 @@ package intro_to_array_lists;
 
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.util.ArrayList;
 
 import javax.swing.JButton;
 import javax.swing.JFrame;
+import javax.swing.JOptionPane;
 import javax.swing.JPanel;
 
 public class GuestBook implements ActionListener {
+	JButton addguest;
+	JButton viewnames;
+	ArrayList<String> guests = new ArrayList<String>();
 	public void run() {
 		// Create a GUI with two buttons. One button reads "Add Name" and the other
 		// button reads "View Names".
@@ -16,15 +21,16 @@ public class GuestBook implements ActionListener {
 		frame.setVisible(true);
 		JPanel panel = new JPanel();
 		frame.add(panel);
-		JButton addbutton = new JButton();
-		addbutton.setText("Add Button");
-		addbutton.addActionListener(this);
+		addguest = new JButton();
+		addguest.setText("Add Guest");
+		addguest.addActionListener(this);
 
-		JButton viewnames = new JButton();
+		viewnames = new JButton();
 		viewnames.setText("View Names");
 		viewnames.addActionListener(this);
 
-		panel.add(addbutton);
+		
+		panel.add(addguest);
 		frame.pack();
 		panel.add(viewnames);
 		frame.pack();
@@ -33,17 +39,23 @@ public class GuestBook implements ActionListener {
 		// that name to an ArrayList. When the "View Names" button is clicked, display a
 		// message dialog that displays
 		// all the names added to the list. Format the list as follows:
-		// Guest #1: Bob Banders
+		// Guest #1: Bob Benders
 		// Guest #2: Sandy Summers
-		// Guest #3: Greg Ganders
-		// Guest #4: Donny Doners
+		// Guest #3: Greg Glanders
+		// Guest #4: Downy Downers
 	}
 
 	@Override
 	public void actionPerformed(ActionEvent e) {
 		JButton buttonClicked = (JButton) e.getSource();
-		if (buttonClicked == addbutton) {
-
+		if (buttonClicked == addguest) {
+			String g = JOptionPane.showInputDialog("Enter a name");
+			guests.add(g);
+		}
+		if(buttonClicked == viewnames) {
+			for(int i=0; i<guests.size();i++) {
+				JOptionPane.showMessageDialog(null, "Guest #"+(i+1)+": "+guests.get(i));
+			}
 		}
 	}
 }
